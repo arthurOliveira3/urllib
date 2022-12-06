cb31e9e6ca0f0574719f255076e8b392e3f795b6
Arthur Oliveira <103527099+arthurOliveira3@users.noreply.github.com>
2022-12-06 09:59:30 -0300

    function workind

--- url.py
+++ url.py
@@ -1,5 +1,34 @@
 import urllib.request
+import time
 
-pagina = urllib.request.urlopen("https://www.gutenberg.org/files/11/11-0.txt")
-texto  = pagina.read().decode("utf8")
-print(texto[0:1000])
+def preco():
+    pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices.html")
+    texto  = pagina.read().decode("utf8")
+    onde  =  texto.find(">$")
+    inicio  = onde + 2
+    fim  = inicio + 4
+    preco  = texto[inicio:fim]
+    preco = float(preco)
+    return preco
+
+
+def precoEspecial():
+    pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
+    texto  = pagina.read().decode("utf8")
+    onde  =  texto.find(">$")
+    inicio  = onde + 2
+    fim  = inicio + 4
+    preco  = texto[inicio:fim]
+    preco = float(preco)
+    return preco
+
+espera = 15
+for n in range(10):
+    if precoEspecial() < 4.70:
+        preco = precoEspecial()
+        print('Comprar! Preço: %5.2f' %preco)
+        time.sleep(espera)
+    else:
+        preco = precoEspecial()
+        print('Espere! Preço: %5.2f' %preco)
+        time.sleep(espera)
