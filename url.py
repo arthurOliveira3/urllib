9aea1682100494699556709528cd8e909229e5b4
Raj IFC <ruhtra.oliveira3@gmail.com>
2022-12-06 22:12:25 -0300

    added the sen email function

--- url.py
+++ url.py
@@ -1,5 +1,10 @@
 import urllib.request
 import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 
 def preco():
     pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices.html")
@@ -12,7 +17,7 @@ def preco():
     return preco
 
 
-def precoEspecial():
+def preco_especial():
     pagina = urllib.request.urlopen("http://beans.itcarlow.ie/prices-loyalty.html")
     texto  = pagina.read().decode("utf8")
     onde  =  texto.find(">$")
@@ -22,13 +27,45 @@ def precoEspecial():
     preco = float(preco)
     return preco
 
-espera = 15
-for n in range(10):
-    if precoEspecial() < 4.70:
-        preco = precoEspecial()
-        print('Comprar! Preço: %5.2f' %preco)
-        time.sleep(espera)
-    else:
-        preco = precoEspecial()
-        print('Espere! Preço: %5.2f' %preco)
-        time.sleep(espera)
+
+def envia_email(preco):
+    email_google = ''
+    senha_google = 'dxxzbjgcgewbvpwr'
+
+    remetente = email_google
+    destinatario = 'INSIRA O EMAIL DO DESTINATARIO'
+
+    mensagem = MIMEMultipart()
+    mensagem['Subject'] = 'Compre o café'
+    mensagem['From'] = remetente
+    mensagem['To'] = destinatario
+    body = f"Você deve comprar o café IMEDIATAMENTE o preço atual é de: {preco}"
+    mensagem.attach(MIMEText(body, 'html'))
+
+    try:
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
+        smtp_server.login(email_google, senha_google)
+        smtp_server.sendmail(remetente, destinatario, str(mensagem))
+        smtp_server.close()
+        print ("E-mail enviado com sucesso!!")
+    except Exception as ex:
+        print ("Algo deu errado...",ex)
+
+
+def main():    
+    espera = 15
+    for n in range(10):
+        if preco_especial() < 4.7:
+            preco = preco_especial()
+            print(f'Comprar! Preço: {preco}')
+            preco = str(preco)
+            envia_email(preco)
+            time.sleep(espera)
+        else:
+            preco = preco_especial()
+            print(f'Espere! Preço: {preco}')
+            time.sleep(espera)
+
+   
+main()
